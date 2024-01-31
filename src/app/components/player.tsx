d669207362ad6b7becf003d21fdc3d937a2e6294
JeromeTT <jerome5139@gmail.com>
2024-01-31 23:33:22 +1100

    Attempted update to player.tsx

--- src/app/components/player.tsx
+++ src/app/components/player.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Hls from "../hls/hls.js";
 const Player = () => {
-  return <div></div>;
+  useEffect(() => {
+    let video = document.getElementById("video");
+    let videoSrc =
+      "https://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8";
+    if (Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+    }
+  }, []);
+  return (
+    <div>
+      <video autoPlay id="my-player" className="video-js" controls></video>
+    </div>
+  );
 };
 
 export default Player;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
-import Script from "next/script";
-import Player from "./components/player";
-import Link from "next/link";
 import Image from "next/image";
 export default function Home() {
   return (
     <html lang="en">
       <body>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <h1 className="text-5xl">CAT.MINTYY.DEV</h1>
-          <Player></Player>
+          <h1 className="text-5xl">Mr. Fresh</h1>
           <Image
             src="https://media1.tenor.com/m/ovWTVvG3VBwAAAAd/mr-fresh-mr-fresh-multiverse.gif"
             width="400"
             height="400"
-            alt="Fresh"
+            alt="Mr. Fresh"
           ></Image>
-          <p>Surely I get this working soon™...</p>
+          <p>(I spent 11 hours just for the stream to not work.)</p>
         </main>
       </body>
     </html>
